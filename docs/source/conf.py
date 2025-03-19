1623f446628c7c8e1e759d96f7d5d03657ea5dbe
Gaspard Macaux <gaspard@2427.cochin.inserm.fr>
2025-03-19 11:37:35 +0100

    Update: Documentation improvements for NicheNet and Circos plot sections

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Gasp'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cell-Hub'
+copyright = '2024, Macaux'
+author = 'Macaux'
 
 release = '0.1'
 version = '0.1.0'
